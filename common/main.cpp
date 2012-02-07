fab4cfbc7d618fc7d547410564f1810b6d008ab6
Luis Gustavo S. Barreto <gustavosbarreto@gmail.com>
2012-02-07 09:56:53 -0200

    Uses a new approach to detect if application is already running

--- common/main.cpp
+++ common/main.cpp
@@ -1,44 +1,37 @@
 #include <QApplication>
-#include <QSharedMemory>
+#include <QStringList>
+#include <QTranslator>
+#include <QLibraryInfo>
 #include <QLocalServer>
 #include <QLocalSocket>
-#include <QStringList>
+#include <QDBusConnection>
+#include <QDebug>
+
+#include "connman.h"
 
 class Application: public QApplication
 {
     Q_OBJECT
 
 public:
-    Application(int &argc, char *argv[], const QString &uniqueKey)
-        : QApplication(argc, argv), m_uniqueKey(uniqueKey)
+    Application(int &argc, char *argv[])
+        : QApplication(argc, argv)
     {
-        m_sharedMemory.setKey(m_uniqueKey);
-        if (m_sharedMemory.attach())
-            m_isRunning = true;
-        else
+        bool isAlreadyRunning = !QDBusConnection::sessionBus().registerService("net.qconnman");
+        if (!isAlreadyRunning)
         {
-            m_isRunning = false;
-            if (!m_sharedMemory.create(1))
-                qFatal("Unable to create single instance");
+            Connman::instance()->init();
 
             m_localServer = new QLocalServer(this);
             connect(m_localServer, SIGNAL(newConnection()), this, SLOT(receiveMessage()));
-            m_localServer->listen(m_uniqueKey);
+            m_localServer->listen("qconnman");
         }
-    }
-
-    bool isRunning()
-    {
-        return m_isRunning;
     }
 
     bool sendMessage(const QString &message)
     {
-        if (!m_isRunning)
-            return false;
-
         QLocalSocket localSocket(this);
-        localSocket.connectToServer(m_uniqueKey, QIODevice::WriteOnly);
+        localSocket.connectToServer("qconnman", QIODevice::WriteOnly);
         if (!localSocket.waitForConnected())
             return false;
 
@@ -63,31 +56,41 @@ public slots:
         localSocket->disconnectFromServer();
     }
 
-signals:
-	void messageAvailable(QString message);
-
 private:
-    bool m_isRunning;
-	QString m_uniqueKey;
-	QSharedMemory m_sharedMemory;
-	QLocalServer *m_localServer;
+    QLocalServer *m_localServer;
+
+signals:
+       void messageAvailable(QString message);
 };
 
 int main(int argc, char *argv[])
 {
-    Application app(argc, argv, "qconnman");
+    Application app(argc, argv);
+    app.setQuitOnLastWindowClosed(false);
+
+    QTranslator qtTranslator;
+    qtTranslator.load("qt_" + QLocale::system().name(),
+                      QLibraryInfo::location(QLibraryInfo::TranslationsPath));
+    app.installTranslator(&qtTranslator);
+
+    QTranslator *appTranslator = new QTranslator;
+    appTranslator->load(QString("%1/../share/qconnman/i18n/%2")
+                        .arg(QCoreApplication::applicationDirPath())
+                        .arg(QLocale::system().name()));
 
     QStringList args = app.arguments();
     args.removeFirst();
 
-    if (!args.isEmpty() && args.at(0).contains(QRegExp("--?applet")))
+    if (!args.isEmpty() && args.at(0).contains(QRegExp("--?manager$")))
+    {
+        qDebug("manager");
+    }
+    else if (!args.isEmpty() && args.at(0).contains(QRegExp("--?applet$")))
     {
         qDebug("applet");
     }
     else
-    {
-        qDebug("manager");
-    }
+        qFatal("Invalid option");
 
     return app.exec();
 }
